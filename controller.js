3d120cd3946ec5e1279a61ba58328dcc21335b16
chancer4 <chance.rhodes5@gmail.com>
2015-11-10 10:36:35 -0500

    More Practice

---
+++ controller.js
@@ -0,0 +1,32 @@
+angular.module('myApp',[]).controller('myController', function($scope){
+	$scope.items = [
+		{
+			name : 'Freddy',
+			desc : 'the mucinex add'
+		},
+		{
+			name : 'Chance',
+			desc : 'the cooliest'
+		},
+		{
+			name : 'Will',
+			desc : 'the wig dude'
+		},
+		{
+			name : 'Stephen',
+			desc : 'the good at this shit'
+		},
+		{
+			name : 'griffin',
+			desc : 'the dota2 addict'
+		},
+		{
+			name : 'oli',
+			desc : 'the hotdick'
+		},
+		{
+			name : 'yohsuke',
+			desc : 'asian wayne gretsky'
+		},
+	];
+});
